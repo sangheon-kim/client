baa9f57a1e1ef8ad9c7fc06086051fd1045af8ef
sangheon-kim <ksj83671@gmail.com>
2022-01-16 22:30:02 +0900

    [김상헌] 접근성 확대 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "src/assets/styles/globals.scss";
+import Head from "next/head";
 import React from "react";
 import wrapper from "src/store/configureStore";
 import type { AppProps } from "next/app";
@@ -6,7 +7,17 @@ import type { AppProps } from "next/app";
 function MyApp(ctx: AppProps) {
   const { Component, pageProps } = ctx;
 
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
