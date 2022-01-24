f085ec4f74d29cb1b982ee0101a9f56d3c0fbdbf
sangheon-kim <ksj83671@gmail.com>
2022-01-24 22:00:10 +0900

    [김상헌] flaw 메인 타이틀 입력

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import type { GetStaticPropsContext, NextPage } from "next";
 import React from "react";
+import Head from "next/head";
 import Link from "next/link";
 // import Calculator from "src/components/calculator/Calculator";
 import wrapper from "src/store/configureStore";
 
 const Home: NextPage = () => {
   return (
-    <h1>
-      {/* <Calculator /> */}
-      Hello,{" "}
-      <Link href="/salary">
-        <a style={{ textDecoration: "underline" }}>급여명세서 발급하러 가기</a>
-      </Link>
-    </h1>
+    <React.Fragment>
+      <Head>
+        <title>Flaw 메인</title>
+      </Head>
+      <h1>
+        {/* <Calculator /> */}
+        Hello,{" "}
+        <Link href="/salary">
+          <a style={{ textDecoration: "underline" }}>
+            급여명세서 발급하러 가기
+          </a>
+        </Link>
+      </h1>
+    </React.Fragment>
   );
 };
 
