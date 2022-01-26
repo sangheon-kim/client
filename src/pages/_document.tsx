f688d61f92b4fc6fb061d6e46fbc3609e1a1b6bd
SangheonKim <62636700+sangheon-kim@users.noreply.github.com>
2022-01-26 15:01:04 +0900

    crossorigin -> crossOrigin
    수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8345292694906055"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
